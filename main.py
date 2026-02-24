ffbb4a6e7169701c15c864dcacff074d8964b0d1
DedeepyaKalavakuri <119920060+dedeepya55@users.noreply.github.com>
2026-02-24 22:14:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ app = FastAPI()
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_DIR = os.path.join(BASE_DIR, "temp_uploads")
 
-OUTPUT_DIR = os.path.join(BASE_DIR, "SMARTSTOCKAI-AI", "output_results")
-
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 
@@ -51,45 +49,6 @@ async def check_arrangement(file: UploadFile = File(...)):
             )
 
         return {"message": "Arrangement check completed"}
-
-    except Exception as e:
-        return JSONResponse(
-            status_code=500,
-            content={"error": str(e)}
-        )
-
-
-# ===============================
-# DEFECT CHECK ENDPOINT
-# ===============================
-@app.post("/defect")
-async def check_defect(file: UploadFile = File(...)):
-    try:
-        file_id = str(uuid.uuid4())
-        image_path = os.path.join(UPLOAD_DIR, f"{file_id}.jpg")
-
-        with open(image_path, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
-
-        script_path = os.path.join(
-            BASE_DIR,
-            "SMARTSTOCKAI-AI",
-            "infer.py"
-        )
-
-        result = subprocess.run(
-            ["python", script_path, image_path],
-            capture_output=True,
-            text=True
-        )
-
-        if result.returncode != 0:
-            return JSONResponse(
-                status_code=500,
-                content={"error": result.stderr}
-            )
-
-        return JSONResponse(content={"response": result.stdout})
 
     except Exception as e:
         return JSONResponse(
